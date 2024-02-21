f41a6920e3bf9e586b4a062dafd8017af0a03f84
forch0 <forchh.dev@gmail.com>
2024-02-21 10:13:42 +0100

    new

--- Bank/settings.py
+++ Bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'useraccess',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -115,9 +119,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = [
+    BASE_DIR/ "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "useraccess:sign-in"
+LOGOUT_REDIRECT_URL = "useraccess:sign-in"
+
+AUTH_USER_MODEL = 'useraccess.User'
+
+JAZZMIN_SETTINGS = {
+   # "site_title": "Library Admin",
+
+    "site_header": "Banklio",
+
+    "site_brand": "Transactions Done Effortlessly......",
+
+    #"site_logo": "books/img/logo.png",
+
+    "copyright": "Banklio - All Right Reserved @ copyright 2024",
+
+
+
+}
